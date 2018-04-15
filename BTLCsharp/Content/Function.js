264f463a421b701aa8baac5619a606cd74729e36
unknown <sanghust97@gmail.com>
2018-04-15 10:47:59 +0700

    Edit 2

--- BTLCsharp/Content/Function.js
+++ BTLCsharp/Content/Function.js
@@ -1,4 +1,13 @@
-﻿function hello() {
-    alert("Hello");
-    window.location.href = "@Url.Action('Index','Home')";
+﻿
+// Function cho nut close 
+
+function closeBtnClicked() {
+    alert("Xin chao ");
+    var response = confirm("Are you want leave page and don't save your work !");
+    if (response === true) {
+        var url = '@Url.Action("Index","Home")';
+        window.location.href = url;
+
+    }
+    return response;
 }
